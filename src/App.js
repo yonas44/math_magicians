ff433019bc96de3594f4f6c85e5aaaa8dd77881b
Yonas44 <tesfu.yonas1350@gmail.com>
2022-12-07 11:42:03 +0300

    Modify notFound page to hide the navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
-import Footer from './components/Footer';
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import NotFound from './components/NotFound';
@@ -11,14 +10,14 @@ function App() {
   return (
     <div className="App-wrapper">
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route element={<Calculator />} path="/math_magicians/calculator" />
-          <Route element={<Homepage />} exact path="/math_magicians/" />
-          <Route element={<Quotes />} exact path="/math_magicians/quotes" />
+          <Route element={<Navbar />} path="/math_magicians/">
+            <Route element={<Calculator />} path="/math_magicians/calculator" />
+            <Route element={<Homepage />} exact path="/math_magicians/" />
+            <Route element={<Quotes />} exact path="/math_magicians/quotes" />
+          </Route>
           <Route element={<NotFound />} exact path="*" />
         </Routes>
-        <Footer />
       </div>
     </div>
   );
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,7 @@ import React from 'react';
 import './styles/NotFound.css';
 
 const NotFound = () => (
-  <div
-    className="notfound-container"
-    style={{ width: '50%', margin: '0 auto' }}
-  >
+  <div className="notfound-container">
     <h2 id="notfound-title">404</h2>
     <p>_Page not found_</p>
   </div>
